834b2140d81dd445d6b356b81d287343b5b8c7a4
whitealex95 <whitealex95@snu.ac.kr>
2022-02-24 13:37:16 -0500

    hopper example perfect

--- towr/test/hopper_example2.cc
+++ towr/test/hopper_example2.cc
@@ -105,39 +105,88 @@ void plan2(ifopt::Problem& nlp, SplineHolder& solution, SplineHolder& last_solut
   solver->Solve(nlp);
 }
 
+void solve_trajectory(ifopt::Problem& nlp, SplineHolder& solution,
+                      State::VectorXd init_base_linear, State::VectorXd init_base_angular,
+                      // double init_x, double init_y, double init_z, double init_r, double init_p, double init_y,
+                      State::VectorXd init_ee_W,
+                      State::VectorXd final_base_linear, State::VectorXd final_base_angular){
+                      // double fin_x, double fin_y, double fin_z,
+                      // double fin_roll, double fin_pitch, double fin_yaw){
+  NlpFormulation formulation;
+  formulation.terrain_ = std::make_shared<FlatGround>(0.0);
+  formulation.model_ = RobotModel(RobotModel::Monoped);
+
+  formulation.initial_base_.lin.at(towr::kPos) << init_base_linear;
+  formulation.initial_base_.ang.at(towr::kPos) << init_base_angular;
+  formulation.initial_ee_W_.push_back(init_ee_W);
+
+  formulation.final_base_.lin.at(towr::kPos) << final_base_linear;
+  formulation.final_base_.ang.at(towr::kPos) << final_base_angular;
+  // formulation.final_base_.lin.at(towr::kPos) << fin_x, fin_y, 0.5;
+  // formulation.final_base_.ang.at(towr::kPos) << fin_roll, fin_pitch, fin_yaw;
+  formulation.params_.ee_phase_durations_.push_back({0.4, 0.2, 0.4, 0.2, 0.4, 0.2, 0.2});
+  formulation.params_.ee_in_contact_at_start_.push_back(true);
+
+  for (auto c : formulation.GetVariableSets(solution))
+    nlp.AddVariableSet(c);
+  for (auto c : formulation.GetConstraints(solution))
+    nlp.AddConstraintSet(c);
+  for (auto c : formulation.GetCosts())
+    nlp.AddCostSet(c);
+  
+  auto solver = std::make_shared<ifopt::IpoptSolver>();
+  solver->SetOption("jacobian_approximation", "exact");
+  solver->SetOption("max_cpu_time", 20.0);
+  solver->Solve(nlp);
+}
 
 int main()
 {
   ::std::vector<ifopt::Problem> nlp_list(2);
   ::std::vector<SplineHolder> solution_list(2);
 
-  plan1(nlp_list[0], solution_list[0], 0, 0, 1, 0);
+  State::VectorXd init_base_linear(3);
+  State::VectorXd init_base_angular(3);
+  State::VectorXd init_ee_W(3);
 
-  // rosbag::Bag bag;
+  ::std::vector<::Eigen::Vector3d> goal_base_linear_list = {{0, 1, -1}, {0, 2, -1}}; // z-position ignored
+  ::std::vector<::Eigen::Vector3d> goal_base_angular_list = {{0, 0, 0}, {0, 0, 0}};
 
 
+  init_base_linear << 0, 0, 0.58; // z-position NOT-ignored
+  init_base_angular << 0, 0, 0;
+  init_ee_W << 0, 0, 0;
+  ::std::cout.precision(2);
+  ::std::cout << "\n>>>>>>\nRunning Optimizer Sequentially\n>>>>>>\n";
+  for (int j=0; j<goal_base_linear_list.size(); j++) {
+    auto goal_base_linear = goal_base_linear_list[j];
+    auto goal_base_angular = goal_base_angular_list[j];
 
-  plan2(nlp_list[1], solution_list[1], solution_list[0], 2, 0);
-  // std::string bag_file = "towr_trajectory.bag";
-  // SaveOptimizationAsRosbag(bag_file, )
-  // plan1(nlp_list[2], solution_list[2], 2, 0, 3, 0);
+    solve_trajectory(nlp_list[j], solution_list[j], // you should directly pass ***_list[j]
+      init_base_linear, init_base_angular, init_ee_W,
+      goal_base_linear, goal_base_angular);
 
-  // Can directly view the optimization variables through:
-  // Eigen::VectorXd x = nlp.GetVariableValues()
-  // However, it's more convenient to access the splines constructed from these
-  // variables and query their values at specific times:
+    auto solution = solution_list[j];
+    auto t_final = solution_list[j].base_linear_->GetTotalTime();
+    auto final_base_linear = solution.base_linear_->GetPoint(t_final).p();
+    auto final_base_angular = solution_list[j].base_angular_->GetPoint(t_final).p();
+    auto final_ee_W = solution_list[j].ee_motion_.at(0)->GetPoint(t_final).p();
+    
+    init_base_linear = final_base_linear;
+    init_base_angular = final_base_angular;
+    init_ee_W = final_ee_W;
+  }
+  ::std::cout << "\n<<<<<\nFinished Optimization\n<<<<<\n";
+
   using namespace std;
-  cout.precision(2);
-  // nlp_list[1].PrintCurrent(); // view variable-set, constraint violations, indices,...
   cout << fixed;
   cout << "\n====================\nMonoped trajectory:\n====================\n";
 
   double t = 0.0;
   double t_start = 0.0;
-  SplineHolder solution;
-  // for (auto solution : solution_list) {
+
   for (int j = 0; j<2; j++){
-    solution = solution_list[j];
+    auto solution = solution_list[j];
     cout << "\n====================\n solution " << j << " :\n====================\n";
     t = 0.0;
     while (t<=solution.base_linear_->GetTotalTime() + 1e-5) {
@@ -158,13 +207,10 @@ int main()
       bool contact = solution.phase_durations_.at(0)->IsContactPhase(t);
       std::string foot_in_contact = contact? "yes" : "no";
       cout << "Foot in contact:              \t" + foot_in_contact << endl;
-
       cout << endl;
-
       t += 0.2;
-
     }
-    cout << "\n###############3? \n" << t << "\n" << solution.base_linear_->GetTotalTime() << "\n################?\n"<< endl;
     t_start += solution.base_linear_->GetTotalTime();
   }
 }
+
